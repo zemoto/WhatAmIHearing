fa9b531c1a16247abfbb3f40c5ee49d1206d5393
Zemoto <none@none.com>
2021-10-10 20:02:17 -0400

    +Record/Stop buttons that trigger recording sys audio to temp wav file

---
+++ WhatAmIHearing/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using NAudio.CoreAudioApi;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using ZemotoUI;
+
+namespace WhatAmIHearing
+{
+   internal enum RecordingState
+   {
+      Stopped = 0,
+      Recording = 1
+   }
+
+   internal sealed class MainViewModel : ViewModelBase
+   {
+      private readonly RecordingManager _recordingManager = new RecordingManager();
+
+      public MainViewModel() => _recordingManager.RecordingStateChanged += ( s, a ) => State = a.NewState;
+
+      public IEnumerable<MMDevice> DeviceList { get; } = new MMDeviceEnumerator().EnumerateAudioEndPoints( DataFlow.All, DeviceState.Active );
+
+      private MMDevice _selectedDevice;
+      public MMDevice SelectedDevice
+      {
+         get => _selectedDevice ??= DeviceList.FirstOrDefault();
+         set => SetProperty( ref _selectedDevice, value );
+      }
+
+      private RecordingState _state;
+      public RecordingState State
+      {
+         get => _state;
+         private set => SetProperty( ref _state, value );
+      }
+
+      private ICommand _recordCommand;
+      public ICommand RecordCommand => _recordCommand ??= new RelayCommand( () => _recordingManager.StartRecording( _selectedDevice ) );
+
+      private ICommand _stopCommand;
+      public ICommand StopCommand => _stopCommand ??= new RelayCommand( _recordingManager.StopRecording );
+   }
+}
---
+++ WhatAmIHearing/RecordingManager.cs
@@ -0,0 +1,54 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System;
+using System.IO;
+using ZemotoUtils;
+
+namespace WhatAmIHearing
+{
+   internal sealed class RecordingStateChangedEventArgs : EventArgs
+   {
+      public RecordingState NewState { get; }
+
+      public RecordingStateChangedEventArgs( RecordingState state ) => NewState = state;
+   }
+
+   internal sealed class RecordingManager
+   {
+      private static readonly string _outputFilePath = Path.Combine( Path.GetTempPath(), "recorded.wav" );
+
+      private WasapiLoopbackCapture _audioCapturer;
+      private WaveFileWriter _audioWriter;
+
+      public event EventHandler<RecordingStateChangedEventArgs> RecordingStateChanged;
+
+      public void StartRecording( MMDevice device )
+      {
+         _audioCapturer = new WasapiLoopbackCapture( device );
+
+         _audioCapturer.DataAvailable += OnDataCaptured;
+         _audioCapturer.RecordingStopped += OnRecordingStopped;
+
+         FileUtils.SafeDeleteFile( _outputFilePath );
+         _audioWriter = new WaveFileWriter( _outputFilePath, _audioCapturer.WaveFormat );
+         _audioCapturer.StartRecording();
+
+         RecordingStateChanged?.Invoke( this, new RecordingStateChangedEventArgs( RecordingState.Recording ) );
+      }
+
+      public void StopRecording() => _audioCapturer.StopRecording();
+
+      private void OnDataCaptured( object sender, WaveInEventArgs e ) => _audioWriter.Write( e.Buffer, 0, e.BytesRecorded );
+
+      private void OnRecordingStopped( object sender, StoppedEventArgs e )
+      {
+         _audioWriter?.Dispose();
+         _audioWriter = null;
+
+         _audioCapturer?.Dispose();
+         _audioCapturer = null;
+
+         RecordingStateChanged?.Invoke( this, new RecordingStateChangedEventArgs( RecordingState.Stopped ) );
+      }
+   }
+}
