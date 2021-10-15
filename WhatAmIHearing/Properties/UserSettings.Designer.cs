3ac74f1f82774be543370c678e2c4e6390ec3259
Zemoto <none@none.com>
2021-10-15 18:39:02 -0400

    Set default "keep open in system tray" option to true

--- WhatAmIHearing/Properties/UserSettings.Designer.cs
+++ WhatAmIHearing/Properties/UserSettings.Designer.cs
@@ -37,7 +37,7 @@ namespace WhatAmIHearing.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool KeepOpenInTray {
             get {
                 return ((bool)(this["KeepOpenInTray"]));
