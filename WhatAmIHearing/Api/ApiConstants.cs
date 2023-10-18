5f8ea45555634e95ac827edd136858322660c75e
David <zemoto@gmail.com>
2023-10-18 14:41:04 -0400

    Add placeholder ApiConstants

---
+++ WhatAmIHearing/Api/ApiConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhatAmIHearing.Api
+{
+   internal static class ApiConstants
+   {
+      public const string SpotifyClientId = "<Placeholder>";
+      public const string SpotifyClientSecret = "<Placeholder>";
+      public const string ShazamApiKey = "<Placeholder>";
+   }
+}
