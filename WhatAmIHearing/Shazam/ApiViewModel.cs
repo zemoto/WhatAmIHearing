6796cb589b79cf1594d3e68ac15a821a239b74e3
David <zemoto@gmail.com>
2024-10-12 12:16:02 -0400

    Save the quota info to file if using a custom api key

--- WhatAmIHearing/Shazam/ApiViewModel.cs
+++ WhatAmIHearing/Shazam/ApiViewModel.cs
@@ -1,5 +1,4 @@
 using CommunityToolkit.Mvvm.ComponentModel;
-using System.Text.Json.Serialization;
 using ZemotoCommon;
 
 namespace WhatAmIHearing.Shazam;
@@ -10,11 +9,9 @@ internal sealed partial class ApiViewModel : ObservableObject
 
    public static ApiViewModel Load() => _keyFile.DeserializeContents<ApiViewModel>() ?? new();
 
-   private bool _keyChanged;
-
    public void Save()
    {
-      if ( _keyChanged )
+      if ( !UseDefaultKey )
       {
          _keyFile.SerializeInto( this );
       }
@@ -24,24 +21,22 @@ internal sealed partial class ApiViewModel : ObservableObject
 
    [ObservableProperty]
    [NotifyPropertyChangedFor( nameof( UseDefaultKey ) )]
+   [NotifyPropertyChangedFor( nameof( CanDisplayQuotaData ) )]
    private string _shazamApiKey;
-   partial void OnShazamApiKeyChanged( string value ) => _keyChanged = true;
 
    public bool UseDefaultKey => string.IsNullOrWhiteSpace( _shazamApiKey );
 
    [ObservableProperty]
    [NotifyPropertyChangedFor( nameof( QuotaUsed ) )]
-   [NotifyPropertyChangedFor( nameof( HaveQuotaData ) )]
-   [property: JsonIgnore]
+   [NotifyPropertyChangedFor( nameof( CanDisplayQuotaData ) )]
    private int _quotaLimit;
 
    [ObservableProperty]
    [NotifyPropertyChangedFor( nameof( QuotaUsed ) )]
-   [NotifyPropertyChangedFor( nameof( HaveQuotaData ) )]
-   [property: JsonIgnore]
+   [NotifyPropertyChangedFor( nameof( CanDisplayQuotaData ) )]
    private int _quotaRemaining;
 
    public int QuotaUsed => _quotaLimit - _quotaRemaining;
 
-   public bool HaveQuotaData => _quotaLimit > 0 && _quotaRemaining >= 0;
+   public bool CanDisplayQuotaData => !UseDefaultKey && _quotaLimit > 0 && _quotaRemaining >= 0;
 }
